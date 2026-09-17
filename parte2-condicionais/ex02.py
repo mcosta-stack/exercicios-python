5d69f2154f2bc632ba54ecffd2578b4c7067c63d
mcosta-stack <murilo_arrazao@estudante.sesisenai.org.br>
2026-09-17 09:25:52 -0300

    exercicio 2  de condicionais

---
+++ parte2-condicionais/ex02.py
@@ -0,0 +1,8 @@
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+if numero1 > numero2:
+    print(f"O maior número é: {numero1}")
+elif numero2 > numero1:
+    print(f"O maior número é: {numero2}")
+else:
+    print("O valor é igual a 0")
